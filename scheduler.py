f87e4758a44a45b7a3605ec3fde38d32e4fe144e
spacepatcher <mycardinalmail@gmail.com>
2017-08-15 15:25:55 +0000

    Deactivate vt scheduling

--- scheduler.py
+++ scheduler.py
@@ -3,13 +3,13 @@ import time
 from modules.hybrid_analysis import ha_run
 from modules.malwr import malwr_run
 from modules.metadefender import metadefender_run
-from modules.virus_total import vt_run
+# from modules.virus_total import vt_run
 
 
 schedule.every(40).minutes.do(ha_run)
 schedule.every(20).minutes.do(malwr_run)
 schedule.every(12).hours.do(metadefender_run)
-schedule.every(10).minutes.do(vt_run)
+# schedule.every(10).minutes.do(vt_run)
 
 
 while True:
